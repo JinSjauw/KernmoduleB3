3196da4dc1fb06a62dc7bb3a35fd784a4a3e9752
SpeedCheese <jinzhouliu4@gmail.com>
2025-03-23 18:34:41 +0100

    Init Commit

---
+++ src/CharacterController.cpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "CharacterController.h"
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/godot.hpp>
+#include <godot_cpp/classes/engine.hpp>
+#include <godot_cpp/classes/input.hpp>
+
+void CharacterController::_bind_methods() 
+{
+    ClassDB::bind_method(D_METHOD("GetMovementSpeed"), &CharacterController::GetMovementSpeed);
+    ClassDB::bind_method(D_METHOD("SetMovementSpeed", "MovementSpeed"), &CharacterController::SetMovementSpeed);
+
+    ClassDB::add_property("CharacterController", PropertyInfo(Variant::FLOAT, "MovementSpeed"), "SetMovementSpeed", "GetMovementSpeed");
+    ADD_SIGNAL(MethodInfo("GetMovementSpeedSignal", PropertyInfo(Variant::FLOAT, "data")));
+}
+
+void CharacterController::_process(double delta) 
+{
+    movementDirection = Vector2(0.0f, 0.0f);
+
+    //Input
+
+    Input& inputSingleton = *Input::get_singleton();
+
+    if(inputSingleton.is_action_pressed("up"))
+    {
+        emit_signal("GetMovementSpeedSignal", movementSpeed);
+        movementDirection.y -= 1.0f;
+    }
+    if(inputSingleton.is_action_pressed("down"))
+    {
+        movementDirection.y += 1.0f;
+    }
+    if(inputSingleton.is_action_pressed("right"))
+    {
+        movementDirection.x += 1.0f;
+    }
+    if(inputSingleton.is_action_pressed("left"))
+    {
+        movementDirection.x -= 1.0f;
+    }
+
+    set_position(get_position() + (movementDirection * movementSpeed * delta));
+}
+
+void CharacterController::SetMovementSpeed(const double speed) 
+{
+    this->movementSpeed = speed;
+}
+
+double CharacterController::GetMovementSpeed() const 
+{
+	return movementSpeed;
+}
+
+CharacterController::CharacterController()
+{
+    if(Engine::get_singleton()->is_editor_hint())
+    {
+        set_process_mode(Node::ProcessMode::PROCESS_MODE_DISABLED);
+    }
+}
+CharacterController::~CharacterController(){}
